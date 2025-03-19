bbd0481bd1faa26debba6463186cf51a7acac3fc
Wichardson <e6207051@cmaisonneuve.qc.ca>
2025-03-18 23:38:45 -0400

    TP1

--- category.php
+++ category.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
+<section class="centrecat">
     <h1><?php single_cat_title();?></h1>
     <p><?php echo category_description(); ?></p>
+</section>
     <section class="populaire">
         <div class="global">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>

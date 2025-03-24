aaa1e5297cb5d15af928634e68add59ed76d721a
Jean-Baptiste <e6207051@cmaisonneuve.qc.ca>
2025-03-24 15:41:03 -0400

    Erreur404

---
+++ 404.php
@@ -0,0 +1,14 @@
+    <?php get_header(); ?>
+    <section class="populaire">
+        <div class="erreur">
+           <h1>Erreur 404</h1>
+
+           <img src="https://img.freepik.com/premium-photo/there-is-pink-airplane-flying-sky-with-clouds-generative-ai_900775-32820.jpg" alt="">
+
+           <h2>La page que vous rechercher ses déja envoler</h2>
+        </div>
+    </section>
+    <?php get_footer(); ?>
+   
+</body>
+</html>

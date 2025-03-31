47a2bea96bfb59a80258fd966df5c4f061f68d84
Jean-Baptiste <e6207051@cmaisonneuve.qc.ca>
2025-03-31 15:37:15 -0400

    image

--- 404.php
+++ 404.php
@@ -1,11 +1,11 @@
     <?php get_header(); ?>
     <section class="populaire">
         <div class="erreur">
-           <h1>Erreur 404</h1>
+           <h1>Oops, vous avez échoué sur l'île 404 !</h1>
 
-           <img src="https://img.freepik.com/premium-photo/there-is-pink-airplane-flying-sky-with-clouds-generative-ai_900775-32820.jpg" alt="">
+           <img src="images/ilepalmier.jpg" alt="">
 
-           <h2>La page que vous rechercher ses déja envoler</h2>
+           <h6>Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d'exception !</h6>
            <div class="entete__navigation">
                 <?php wp_nav_menu(array(
                     'menu' => '404',

e91800f388ca24ebc79d191d06033d9215545e58
Jean-Baptiste <e6207051@cmaisonneuve.qc.ca>
2025-05-23 10:54:32 -0400

    essai svg2

--- JS/destination.js
+++ JS/destination.js
@@ -4,15 +4,15 @@
 (function(){
     console.log("destination.js")
     let categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
-    // const domaine = document.querySelector('base').href;
+    const domaine = document.querySelector('base').href;
     /* la technique utilisée pour extraire l'url doit êtere généralisée */
-    const domaine = window.origin + "/4w4_03/";
+    //const domaine = window.origin + "/4w4_03/pays/"
 
     parcourir_bouton()
     mon_fetch(categoryId)
 
 function parcourir_bouton(){
-    const categorie__ul__li = document.querySelectorAll(".categorie__ul__li")
+    const categorie__ul__li = document.querySelectorAll('.categorie__ul__li[data-methad="categorie"]');
     console.log("categorie__ul__li.length = ", categorie__ul__li.length)
     categorie__ul__li.forEach(elm => {
         elm.addEventListener('mousedown', function(){
